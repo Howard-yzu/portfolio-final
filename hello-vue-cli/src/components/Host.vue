de47ac8fb5b8a368b97f60029f9cd027810d6e0f
Howard <a0903571712@gmail.com>
2022-06-15 02:04:54 +0800

    [feat]:run build

---
+++ hello-vue-cli/src/components/Host.vue
@@ -0,0 +1,93 @@
+<template>
+  <section>
+    <div class="nav -bar"></div>
+    <h1>Galaxy Tours</h1>
+    <div>
+      <h2>{{ cruise.name }}</h2>
+      <div>{{ cruise.description }}</div>
+      <hr />
+      <div class="row">
+        <div>
+          <!-- TODO: Add booking-form -->
+          <booking-form
+            @bookingCreated="addBooking"
+            :cabins="cruise.cabins"
+          ></booking-form>
+        </div>
+        <div>
+          <!-- TODO: Add booking-list -->
+          <booking-list :bookings="bookings"></booking-list>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+import BookingList from "./BookingList.vue";
+import BookingForm from "./BookingForm.vue";
+//TODO: Register next component
+export default {
+  data() {
+    return {
+      cruise: {
+        name: "Cruise to the moon",
+        description:
+          "Cruise to the moon in our luxurious shuttle. Watch the astronauts working outside the International Space Station.",
+        cabins: [
+          { name: "Coach", price: 125000 },
+          { name: "Business", price: 275000 },
+          { name: "First", price: 430000 },
+        ],
+      },
+      bookings: [{ name: "Sample", price: 0 }],
+    };
+  },
+  // TODO: Add components
+  components: {
+    BookingList,
+    BookingForm,
+
+    // TODO: Add next component
+  },
+
+  // TODO: Add methods
+  methods: {
+    addBooking(cabinIndex) {
+      const cabin = this.cruise.cabins[cabinIndex];
+      const booking = {
+        cabin: cabin.name,
+        price: cabin.price,
+      };
+      this.bookings.push(booking);
+    },
+  },
+};
+</script>
+
+<style>
+body {
+  background-color: #f2f2f2;
+  margin: 0, 5%;
+  font-family: tahoma;
+}
+.row {
+  display: grid;
+  grid-template-columns:
+    1fr
+    1fr;
+  vertical-align: middle;
+  margin: 2em;
+}
+.button {
+  background-color: #39495c;
+  border-radius: 5px;
+  color: white;
+  text-align: center;
+}
+.nav -bar {
+  background: linear -gradient (-50deg, #010801, #0d0d60);
+  height: 60px;
+  margin-bottom: 25px;
+}
+</style>
